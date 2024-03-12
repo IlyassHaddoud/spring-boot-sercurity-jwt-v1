d7b22780e6071fbe3d8b3139ff10bf00c1e2e07f
Ilyass <ilyass.haddoud@gmail.com>
2024-03-12 11:11:16 +0000

    adding global mysql exception handler controller

--- src/main/java/com/jwt/api/user/User.java
+++ src/main/java/com/jwt/api/user/User.java
@@ -1,18 +1,16 @@
 package com.jwt.api.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
