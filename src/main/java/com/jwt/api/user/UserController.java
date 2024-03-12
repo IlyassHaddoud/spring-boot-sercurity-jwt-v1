274df7d13fcb4a969a3c315b837217ef1d383c4c
Ilyass <ilyass.haddoud@gmail.com>
2024-03-12 11:36:45 +0000

    adding UserLoginDTO and ValidationExceptionHandler controller advisor

--- src/main/java/com/jwt/api/user/UserController.java
+++ src/main/java/com/jwt/api/user/UserController.java
@@ -1,9 +1,11 @@
 package com.jwt.api.user;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -19,6 +21,12 @@ public class UserController {
     public List<User> Home()
     {
         return this.userService.getUsers();
+    }
+
+    @PostMapping("login")
+    public Optional<User> login(@Valid @RequestBody UserLoginDTO userLoginDTO)
+    {
+        return this.userService.login(userLoginDTO.getEmail());
     }
     @PostMapping("register")
     public User register(@RequestBody User user)
--- src/main/java/com/jwt/api/user/UserService.java
+++ src/main/java/com/jwt/api/user/UserService.java
@@ -1,17 +1,21 @@
 package com.jwt.api.user;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
     public List<User> getUsers()
@@ -19,8 +23,18 @@ public class UserService {
         return this.userRepository.findAll();
     }
 
+    public Optional<User> login(String email)
+    {
+        return this.userRepository.getUserByEmail(email);
+    }
+
     public User register(User user)
     {
         return this.userRepository.save(user);
+    }
+
+    public User convertToUser(UserLoginDTO userLoginDTO)
+    {
+        return this.modelMapper.map(userLoginDTO,User.class);
     }
 }
